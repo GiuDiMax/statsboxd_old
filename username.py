7dca9c418fec78cc1cc821c70888bf6e70e1fb7e
GiuDiMax <giuppydimauro@hotmail.it>
2022-05-25 17:26:24 +0200

    piccole modifiche

--- username.py
+++ username.py
@@ -4,7 +4,8 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 from mongodb import db
 from datetime import datetime
-from fullProcedure import fullUpdate, fullCreation
+from stats import getStats, getLists
+from operations import fillMongodb
 
 global listx
 
@@ -96,5 +97,66 @@ def getFromusername(username):
         return db.Users.find_one({"username": username})
     except:
         return None
+
+
+def fullCreation(username):
+    watched_list = get_watched(username, False)
+    diary_list = get_watched(username, True)
+    db.Users.insert_one({'username': username, 'watched': watched_list, 'diary': diary_list})
+    fullUpdate(username)
+
+
+def fullUpdate(username):
+    username_object = getFromusername(username)
+    watched = username_object['watched']
+    ids = []
+    uris = []
+    for movie in watched:
+        ids.append(movie['id'])
+        uris.append(movie['uri'])
+
+    db.tmpUris.delete_many({})
+    db.tmpUris.insert_many(username_object['watched'])
+
+    while True:
+        obj1 = db.Film.find({"_id": {"$in": ids}})
+        uris2 = list(set(uris) - set(obj1.distinct('uri')))
+        print(len(uris2))
+        if len(uris2) > 0:
+            try:
+                fillMongodb(uris2)
+            except:
+                pass
+        else:
+            break
+
+    print('names')
+    #mainSetNames2()
+    json3 = getStats(username)
+    for x in json3:
+        y = x
+
+    min = y['totalyear'][0]['_id']
+    max = y['totalyear'][-1]['_id']
+
+
+    y2 = []
+    for i in range(min, max + 1):
+        check = False
+        for a in y['totalyear']:
+            if a['_id'] == i:
+                y2.append(a)
+                check = True
+                break
+        if not check:
+            y2.append({'_id': i, 'average': 0, 'sum': 0})
+    y['totalyear'] = y2
+
+    x = []
+    json4 = getLists()
+    for i in json4:
+        x.append(i)
+    y = y | {'lists': x}
+    db.Users.update_one({'username': username}, {'$set': {'stats': y}})
 
 
