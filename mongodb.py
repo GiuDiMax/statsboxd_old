5c813644efe5911e1749005c24cf925fdffc21d5
GiuDiMax <giuppydimauro@hotmail.it>
2022-05-31 10:22:33 +0200

    some stuff

--- mongodb.py
+++ mongodb.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from config import client_string
 from pprint import pprint
 
-client = MongoClient(client_string)
+client = MongoClient(client_string, connect=False)
 db = client.Letterboxd
 #film = {'_id': 0, 'a': '2', 'b': '3'}
 #result = db.Film.insert_one(film)
