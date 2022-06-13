745c8a0cdaa35da7ef4c58642bc40721e8bc2d3a
GiuDiMax <giuppydimauro@hotmail.it>
2022-06-13 11:07:59 +0200

    some css things

--- otherStuff.py
+++ otherStuff.py
@@ -1,10 +1,29 @@
 from mongodb import db
 import time
+from datetime import date, timedelta, datetime
+from operations import fillMongodb
 
 #db.Users.delete_many({})
 #db.Film.delete_many({})
 
-db.Film.delete_many({ 'images': { '$exists': False }})
+#db.Film.delete_many({ 'images': { '$exists': False }})
+
+datex = datetime.today()
+datex = datex - timedelta(days=7)
+
+current_year = date.today().year
+a = db.Film.aggregate([
+    {'$match': {"year": {'$gt': current_year - 2}}},
+    {'$match': {"updateDate": {'$lt': datex}}},
+    {'$project': {'uri': 1}}
+])
+
+uris = []
+for x in a:
+    uris.append(x['uri'])
+
+print(len(uris))
+fillMongodb(uris)
 
 '''
 print(1)
