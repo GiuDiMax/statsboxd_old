d3ace0923f01b01070174d800f5c3defa0ba6096
GiuDiMax <giuppydimauro@hotmail.it>
2022-06-15 10:23:38 +0200

    some stuff

--- utils/otherStuff2.py
+++ utils/otherStuff2.py
@@ -2,18 +2,25 @@ from mongodb import db
 from datetime import date, timedelta, datetime
 from operations import fillMongodb
 
+
 def refresh():
-    a = db.Film.aggregate([
-        {'$match': {'runtime': {'$exists': False}}},
-        {'$project': {'uri': 1}}
+    a = db.Users.aggregate([
+        {'$match': {"_id": 'alexmod1'}},
+        {'$unwind': '$watched'},
+        {'$lookup': {
+            'from': 'Film',
+            'localField': 'watched.id',
+            'foreignField': '_id',
+            'as': 'info'}},
+        {'$unwind': '$info'},
+        {'$match': {'info.runtime': {'$exists': False}}},
+        {'$group': {'_id': '$_id',
+                    'movies': {'$sum': 1},
+                    'runtime': {'$sum': '$info.runtime'}}}
     ])
 
-    uris = []
     for x in a:
-        uris.append(x['uri'])
-
-    print(len(uris))
-    fillMongodb(uris)
+        print(x)
 
 
 if __name__ == '__main__':
