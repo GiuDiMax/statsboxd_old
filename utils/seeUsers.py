21d6f53091dd30f25629e4bc742f431e9ffcd746
GiuDiMax <giuppydimauro@hotmail.it>
2022-06-21 11:07:36 +0200

    some stuff

--- utils/seeUsers.py
+++ utils/seeUsers.py
@@ -1,7 +1,19 @@
 from mongodb import db
+from utils.cleanUsers import cleanUsers
 
-a = db.Users.aggregate([
-    {'$project': {'_id': 1, 'name': 1}},
-])
-for x in a:
-    print(x)
+
+def seeUsers():
+    cleanUsers()
+    a = db.Users.aggregate([
+        {'$project': {'_id': 1, 'name': 1}},
+    ])
+    i = 0
+    for x in a:
+        i = i+1
+        print(x)
+    print('total: ' + str(i))
+
+
+if __name__ == '__main__':
+    seeUsers()
+
