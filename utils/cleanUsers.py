b1f1005b0a300c9d23d8798d3508cde33be435b5
GiuDiMax <giuppydimauro@hotmail.it>
2022-06-23 19:04:42 +0200

    some stuff

--- utils/cleanUsers.py
+++ utils/cleanUsers.py
@@ -2,8 +2,8 @@ from mongodb import db
 from operations import fillMongodb
 
 def cleanUsers():
-    #db.Users.delete_many({})
-    db.Users.delete_many({'name': {'$exists': False}})
+    db.Users.delete_many({})
+    #db.Users.delete_many({'name': {'$exists': False}})
 
 if __name__ == '__main__':
     cleanUsers()
