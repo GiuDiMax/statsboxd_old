94570a89ac6d7a8c2dba4ccb8b9ee0e553a95b99
GiuDiMax <giuppydimauro@hotmail.it>
2022-06-29 15:38:46 +0200

    some stuff

--- utils/cleanUsers.py
+++ utils/cleanUsers.py
@@ -10,6 +10,7 @@ def cleanUsers():
     db.Users.update_many({}, {'$unset': {'diary': 1, 'stats': 1,
                                          'stats_2022': 1, 'stats_2021': 1, 'stats_2020': 1,
                                          'stats_2019': 1, 'stats_2018': 1, 'stats_2017': 1,
+                                         'stats_2016': 1, 'stats_2015': 1, 'stats_2014': 1,
                                          'years': 1, 'sug': 1, 'mostWatched': 1, 'image': 1,
                                          }})
 
