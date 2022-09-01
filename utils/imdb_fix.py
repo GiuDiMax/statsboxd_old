ab4ff592291c31e06ffd4553973b80507e3ebe10
GiuDiMax <giuppydimauro@hotmail.it>
2022-09-01 16:23:57 +0200

    some stuff

--- utils/imdb_fix.py
+++ utils/imdb_fix.py
@@ -1,18 +1,35 @@
 from mongodb import db
+from operations import fillMongodb
 
 
 def fix():
     a = db.Film.aggregate([
-        {'$match': {"imdb": {'$lt': 1000000}}},
+        {'$match': {"imdb": {'$exists': True}}},
+        {'$match': {"tt": {'$nin': 'imdb'}}},
         {'$limit': 1000},
         {'$project': {'uri': 1, 'imdb': 1}}
     ])
 
     uris = []
     for x in a:
-        y = str(x['imdb']).zfill(7)
+        y = 'tt' + str(x['imdb']).zfill(7)
         db.Film.update_one({'_id': x['_id']}, {'$set': {'imdb': y}}, True)
     print('1000 fatti')
+
+
+def fix2():
+    a = db.Film.aggregate([
+        {'$match': {"tmdb_tv": {'$exists': False}}},
+        {'$match': {"tmdb": {'$exists': False}}},
+        #{'$limit': 1000},
+        {'$project': {'uri': 1}}
+    ])
+    uris = []
+    for x in a:
+        uris.append(x['uri'])
+    #print(uris)
+    print(len(uris))
+    fillMongodb(uris)
 
 
 if __name__ == '__main__':
