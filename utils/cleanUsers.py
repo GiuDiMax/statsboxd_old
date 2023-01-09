2afedc703bae9e6f40ee4d43102a5fe24ec99c60
GiuDiMax <giuppydimauro@hotmail.it>
2023-01-09 10:23:52 +0100

    some stuff

--- utils/cleanUsers.py
+++ utils/cleanUsers.py
@@ -7,7 +7,7 @@ def cleanUsers():
     #createAlgo()
     #db.Users.delete_many({})
     db.Users.delete_many({'name': {'$exists': False}})
-    db.Users.update_many({}, {'$unset': {'diary': 1, 'stats': 1,
+    db.Users.update_many({}, {'$unset': {'diary': 1, 'stats': 1, 'stats_2023': 1,
                                          'stats_2022': 1, 'stats_2021': 1, 'stats_2020': 1,
                                          'stats_2019': 1, 'stats_2018': 1, 'stats_2017': 1,
                                          'stats_2016': 1, 'stats_2015': 1, 'stats_2014': 1,
