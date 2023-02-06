5b5768cbdb81001360c0b0f9dff3c7fce300d9e5
GiuDiMax <giuppydimauro@hotmail.it>
2023-02-06 09:34:59 +0100

    some stuff

--- year_stats.py
+++ year_stats.py
@@ -95,5 +95,5 @@ def year_stats(username, fastUpdate):
     #years.sort(reverse=True)
         db.Users.update_one({'_id': username}, {'$set': {'years': years}})
 
-singleYear(2022, 'giudimax')
+#singleYear(2022, 'giudimax')
 #year_stats('giudimax')
