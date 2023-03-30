fe164f3d1b6dd4eb9e983c2e67156983f7a81645
GiuDiMax <giuppydimauro@hotmail.it>
2023-03-30 12:11:00 +0200

    some stuff

--- utils/updateData.py
+++ utils/updateData.py
@@ -1,6 +1,6 @@
 from mongodb import db
 from datetime import date, timedelta, datetime
-from operations import fillMongodb, fillMongodbmembers
+from operations import fillMongodb, fillMongodbmembers, fillMongodbratings
 import time
 from setLists import updateLists
 from setPeople import mainSetNames
@@ -26,27 +26,13 @@ def refresh(i):
     uris = []
     for x in a:
         uris.append(x['uri'])
-    print('analyizing ' + str(len(uris) + i*1000))
+    print('analyizing data' + str(len(uris) + i*1000))
     fillMongodb(uris)
 
 
-def refreshata(i):
-    try:
-        refresh(i)
-    except:
-        time.sleep(10)
-        refreshata(i)
-
-
 def addMembers(i):
-    datex = datetime.today()
-    #datex = datex - timedelta(days=10)
-    datex = datex - timedelta(hours=24)
-    current_year = date.today().year
-
     a = db.Film.aggregate([
-        {'$match': {"members": {'$exists': 0}}},
-        {'$sort': {'updateDate': 1}},
+        {'$sort': {'updateMembers': 1}},
         {'$limit': 1000},
         {'$project': {'uri': 1}}
     ])
@@ -54,29 +40,45 @@ def addMembers(i):
     uris = []
     for x in a:
         uris.append(x['uri'])
-    print('analyizing ' + str(len(uris) + i*1000))
+    print('analyzing members ' + str(len(uris) + i*1000))
     fillMongodbmembers(uris)
 
 
-def refreshata2(i):
+def addRatings(i):
+    a = db.Film.aggregate([
+        {'$sort': {'updateRating': 1}},
+        {'$limit': 1000},
+        {'$project': {'uri': 1}}
+    ])
+
+    uris = []
+    for x in a:
+        uris.append(x['uri'])
+    print('analyizing ratings ' + str(len(uris) + i*1000))
+    fillMongodbratings(uris)
+
+
+def refreshata(i, k):
     try:
-        addMembers(i)
+        if k == 0:
+            refresh(i)
+        elif k == 1:
+            addMembers(i)
+        elif k == 2:
+            addRatings(i)
     except:
         time.sleep(10)
-        refreshata2(i)
+        refreshata(i, k)
 
 
 if __name__ == '__main__':
-    for i in range(78):
-        start = time.time()
-        refreshata(i)
-        print('Done in ' + str(time.time() - start))
+    for k in range(1, 3):
+        for i in range(78):
+            start = time.time()
+            refreshata(i, k)
+            print('Done in ' + str(time.time() - start))
 
-    for i in range(50):
-        start = time.time()
-        refreshata2(i)
-        print('Done in ' + str(time.time() - start))
-
+    exit()
     all()
     updateLists()
     mainSetNames()
