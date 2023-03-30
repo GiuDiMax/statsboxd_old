44e1955b66ed9c67c7cc65e5a6071cd806ad3fa6
GiuDiMax <giuppydimauro@hotmail.it>
2023-03-30 17:09:33 +0200

    some stuff

--- utils/tmdb_new_update.py
+++ utils/tmdb_new_update.py
@@ -2,9 +2,22 @@ import requests
 from datetime import datetime, timedelta
 from operations import fillMongodb, fillMongodbmembers, fillMongodbratings
 from mongodb import db
+from threading import Thread
+
+global ids
+
+
+def getids(url, pag, start, end):
+    global ids
+    r = requests.get(url.format(page=pag, start=start, end=end)).json()
+    for id in r['results']:
+        ids.append(id['id'])
 
 
 def updatefromtmdb(onlytoday, dayoff=1):
+    global ids
+    ids = []
+
     nowx = datetime.now() - timedelta(days=dayoff)
     past = (nowx - timedelta(days=14)).isoformat()
     if onlytoday:
@@ -14,23 +27,31 @@ def updatefromtmdb(onlytoday, dayoff=1):
     urlx = url.format(page=1, start=past, end=now)
     r = requests.get(urlx).json()
     pages = int(r['total_pages'])
-    for j in range(pages):
-        ids = []
+    th = []
+    for j in range(pages+1):
+        th.append(Thread(target=getids, args=(url, j+1, past, now,)))
+    for t in th:
+        t.start()
+    for t in th:
+        t.join()
+
+    while True:
         uris = []
-        urlx = url.format(page=j+1, start=past, end=now)
-        r = requests.get(urlx).json()
-        for id in r['results']:
-            ids.append(id['id'])
         a = db.Film.aggregate([
             {'$match': {"tmdb": {'$in': ids}}},
             {'$match': {"updateDate": {'$lt': nowx}}},
-            {'$project': {'_id': 0, 'uri': 1}}
+            {'$project': {'_id': 0, 'uri': 1}},
+            {'$limit': 100}
         ])
         for x in a:
             uris.append(x['uri'])
-        fillMongodb(uris)
-        fillMongodbmembers(uris)
-        fillMongodbratings(uris)
+        if len(uris) == 0:
+            break
+        else:
+            print(len(uris))
+            fillMongodb(uris)
+            fillMongodbmembers(uris)
+            fillMongodbratings(uris)
 
 
 if __name__ == '__main__':
