40606678dbb3038d199f38a11f4cd99694f9f389
GiuDiMax <giuppydimauro@hotmail.it>
2023-03-30 16:04:22 +0200

    some stuff

--- utils/updateData.py
+++ utils/updateData.py
@@ -12,6 +12,7 @@ limitx = 100
 
 
 def refresh(i, nn):
+    b = False
     datex = datetime.today()
     datex = datex - timedelta(hours=24)
     current_year = date.today().year
@@ -27,11 +28,15 @@ def refresh(i, nn):
     uris = []
     for x in a:
         uris.append(x['uri'])
+    if len(uris)<limitx:
+        b = True
     print('analyizing data' + str(len(uris) + i*limitx))
     fillMongodb(uris)
+    return b
 
 
 def addMembers(i, nn):
+    b = False
     datex = datetime.today()
     datex = datex - timedelta(hours=24)
     current_year = date.today().year
@@ -39,6 +44,7 @@ def addMembers(i, nn):
     a = db.Film.aggregate([
         {'$match': {"year": {'$gt': current_year - nn}}},
         {'$match': {'$or': [{"updateMembers": {'$lt': datex}}, {'updateMembers': {'$exists': False}}]}},
+        #{'$match': {'members': {'$exists': False}}},
         {'$sort': {'updateMembers': 1}},
         {'$limit': limitx},
         {'$project': {'uri': 1}}
@@ -47,11 +53,15 @@ def addMembers(i, nn):
     uris = []
     for x in a:
         uris.append(x['uri'])
+    if len(uris)<limitx:
+        b = True
     print('analyzing members ' + str(len(uris) + i*limitx))
     fillMongodbmembers(uris)
+    return b
 
 
 def addRatings(i, nn):
+    b = False
     datex = datetime.today()
     datex = datex - timedelta(hours=24)
     current_year = date.today().year
@@ -59,6 +69,7 @@ def addRatings(i, nn):
     a = db.Film.aggregate([
         {'$match': {"year": {'$gt': current_year - nn}}},
         {'$match': {'$or': [{"updateRating": {'$lt': datex}}, {'updateRating': {'$exists': False}}]}},
+        #{'$match': {'ratings': {'$exists': False}}},
         {'$sort': {'updateRating': 1}},
         {'$limit': limitx},
         {'$project': {'uri': 1}}
@@ -67,21 +78,26 @@ def addRatings(i, nn):
     uris = []
     for x in a:
         uris.append(x['uri'])
+    if len(uris)<limitx:
+        b = True
     print('analyizing ratings ' + str(len(uris) + i*limitx))
     fillMongodbratings(uris)
+    return b
 
 
 def refreshata(i, k, nn):
+    b = False
     try:
         if k == 0:
-            refresh(i, nn)
+            b = refresh(i, nn)
         elif k == 1:
-            addMembers(i, nn)
+            b = addMembers(i, nn)
         elif k == 2:
-            addRatings(i, nn)
+            b = addRatings(i, nn)
     except:
         time.sleep(15)
-        refreshata(i, k, nn)
+        b = refreshata(i, k, nn)
+    return b
 
 
 if __name__ == '__main__':
@@ -89,7 +105,8 @@ if __name__ == '__main__':
     for k in range(1, 3):
         for i in range(int(80000/limitx)+1):
             #start = time.time()
-            refreshata(i, k, nn)
+            b = refreshata(i, k, nn)
+            if b: break
             #print('Done in ' + str(time.time() - start))
 
     exit()
