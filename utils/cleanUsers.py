f86aa25337a8f8960ee127d731cfa198bc096159
GiuDiMax <giuppydimauro@hotmail.it>
2023-04-01 15:34:49 +0200

    some stuff

--- utils/cleanUsers.py
+++ utils/cleanUsers.py
@@ -1,10 +1,7 @@
 from mongodb import db
-from operations import fillMongodb
-#from recommend.multiUsers import createAlgo
 
 
 def cleanUsers():
-    #createAlgo()
     #db.Users.delete_many({})
     db.Users.delete_many({'name': {'$exists': False}})
     db.Users.update_many({}, {'$unset': {'diary': 1, 'stats': 1, 'stats_2023': 1,
@@ -12,7 +9,8 @@ def cleanUsers():
                                          'stats_2019': 1, 'stats_2018': 1, 'stats_2017': 1,
                                          'stats_2016': 1, 'stats_2015': 1, 'stats_2014': 1,
                                          'years': 1, 'mostWatched': 1, 'image': 1, 'sug2': 1,
-                                         'diaryperyear': 1, 'extra_stats': 1, 'diary2': 1
+                                         'diaryperyear': 1, 'extra_stats': 1, 'diary2': 1, 'sug3': 1,
+                                         'sug_list': 0, 'each': 0,
                                          }})
 
 
