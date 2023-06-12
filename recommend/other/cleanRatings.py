fa20f49124586adf231ed0c8c403130a308dbfba
GiuDiMax <giuppydimauro@hotmail.it>
2023-06-12 10:36:25 +0200

    add some files

---
+++ recommend/other/cleanRatings.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def cleanRatings(df, single=False):
+    df[['userId']] = df[['userId']].apply(pd.to_numeric, errors='coerce')
+    df[['movieId']] = df[['movieId']].apply(pd.to_numeric, errors='coerce')
+    df[['rating']] = df[['rating']].apply(pd.to_numeric, errors='coerce')
+    df = df.dropna().astype(int).drop_duplicates()
+    movies = pd.read_csv('movies.csv', low_memory=False, error_bad_lines=False)
+    df = pd.merge(df, movies, on='movieId', how='right')
+    df = df.drop(columns=['uri']).dropna().sort_values(by=['userId'])
+    '''
+    df['rating'] = df['rating'] - df['average']/5
+    df['rating'] = df['rating'].round(1)
+    '''
+    return df
+
+
+if __name__ == '__main__':
+    dfx = pd.read_csv('ratings.csv', low_memory=False, error_bad_lines=False, encoding_errors='ignore')
+    cleanRatings(dfx).to_csv('ratings_clean.csv', index=False)
