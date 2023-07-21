0b101ddb56369693f603d09347ded14069634af1
GiuDiMax <giuppydimauro@hotmail.it>
2023-07-21 15:47:35 +0200

    some edit

--- utils/test.py
+++ utils/test.py
@@ -4,21 +4,12 @@ from time import time
 
 def test():
     start = time()
-    obj = list(db.Users.aggregate([
-        {'$match': {'_id': 'giudimax'}},
-        {'$unwind': '$watched'},
-        {'$group': {'_id': '$watched.id'}},
-        {'$lookup': {'from': 'Film', 'localField': '_id', 'foreignField': '_id', 'as': 'info'}},
-        {'$project': {'_id': 0, 'uri': '$info.uri', 'ad': '$info.actors'}},
-        {'$unwind': '$uri'},
-        {'$unwind': '$ad'},
-        {'$unwind': '$ad'},
-        {'$group': {'_id': '$ad', 'sum': {'$sum': 1}, 'list': {'$push': '$uri'}}},
-        {'$sort': {'sum': -1}},
-        {'$limit': 10}
+    obj = list(db.Film.aggregate([
+        {'$match': {'year': 2022}},
+        {'$project': {'_id': 1}}
     ]))
-    print(obj)
-    print(time()-start)
+    print(len(obj))
+    print(time() - start)
 
 
 if __name__ == '__main__':
